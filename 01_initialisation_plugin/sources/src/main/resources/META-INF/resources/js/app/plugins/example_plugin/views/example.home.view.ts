729dcd96b58673d481edaa71307801defa79b476
Patrick Guillerm <patrickguillerm@gmail.com>
2019-08-15 20:33:26 +0200

    append formation sources code

---
+++ 01_initialisation_plugin/sources/src/main/resources/META-INF/resources/js/app/plugins/example_plugin/views/example.home.view.ts
@@ -0,0 +1,25 @@
+// ANGULAR CORE MODULES --------------------------------------------------------
+import {Component, OnInit}                     from '@angular/core';
+
+// SERVICES --------------------------------------------------------------------
+@Component({
+    templateUrl: 'js/app/plugins/example_plugin/views/example.home.view.html',
+    directives : [
+    ]
+})
+export class ExampleHomeView implements OnInit{
+  /**************************************************************************
+  * ATTRIBUTES
+  **************************************************************************/
+ 
+  
+  /**************************************************************************
+  * CONSTRUCTORS
+  **************************************************************************/
+  constructor(){
+  }
+
+  ngOnInit() {
+   
+  }
+}
